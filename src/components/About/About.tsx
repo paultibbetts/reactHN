ac477f560f20e25d700553e4745e48b246048558
Paul Tibbetts <code@paultibbetts.uk>
2020-07-11 17:08:36 +0100

    refactor: change plugins

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,9 +3,15 @@ import { setTitle } from '../../helpers';
 import raw from "raw.macro";
 import unified from 'unified'
 import parse from 'remark-parse'
-import remark2react from 'remark-react'
+import remark2rehype from 'remark-rehype';
+import rehype2react from 'rehype-react'
 
 const markdown = raw("../../../README.md");
+
+const processor = unified()
+.use(parse)
+.use(remark2rehype)
+.use(rehype2react, {createElement: React.createElement});
 
 export const About: React.FC = () => {
   useEffect(() => {
@@ -14,12 +20,7 @@ export const About: React.FC = () => {
 
   return (
     <div className="About">
-      {
-        unified()
-          .use(parse)
-          .use(remark2react)
-          .processSync(markdown).result
-      }
+      {processor.processSync(markdown).result}
       <p className="dark:text-white">
         View the source code on <a className="dark:hover:text-gray-700" href="https://github.com/ptibbetts/reactHN">GitHub</a>
       </p>
