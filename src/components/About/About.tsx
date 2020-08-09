bdbee1451ba19dfdeda638de48d3ada5f4d497d9
Paul Tibbetts <code@paultibbetts.uk>
2020-08-09 16:39:03 +0100

    feat: change about page to read readme markdown (#22)
    * feat: change about page to read readme markdown
    
    * fix: missing remark-react package
    
    that I swear I installed and saw it working
    
    * fix: build not working because of strict typescript
    
    * refactor: change plugins
    
    * refactor: change to remarkable for typescript support

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from 'react';
 import { setTitle } from '../../helpers';
+import raw from 'raw.macro';
+import Markdown from 'react-remarkable';
+const markdown = raw("../../../README.md");
 
 export const About: React.FC = () => {
   useEffect(() => {
@@ -7,14 +10,8 @@ export const About: React.FC = () => {
   })
 
   return (
-    <div className="bg-white dark:bg-gray-600 p-2">
-      <h2 className="text-xl font-bold mb-4 dark:text-white">About</h2>
-      <p className="dark:text-white">
-        Yet another Hacker News clone
-      </p>
-      <p className="dark:text-white">
-        Featuring <a className="dark:hover:text-gray-700" href="https://reactjs.org/">React 16</a>, <a className="dark:hover:text-gray-700" href="https://reacttraining.com/react-router/">React Router 4</a>, <a className="dark:hover:text-gray-700" href="https://react-redux.js.org/">React Redux</a>, <a className="dark:hover:text-gray-700" href="https://github.com/gaearon/redux-thunk">Redux Thunk</a>, <a className="dark:hover:text-gray-700" href="https://github.com/cheeaun/node-hnapi/">HN API</a>, unread link highlighting, clickable links in comments, dark mode, TailwindCSS, Storybook and playroom.
-      </p>
+    <div className="About">
+      <Markdown source={markdown} />
       <p className="dark:text-white">
         View the source code on <a className="dark:hover:text-gray-700" href="https://github.com/ptibbetts/reactHN">GitHub</a>
       </p>
